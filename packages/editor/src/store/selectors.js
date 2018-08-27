ec9c4d0fed75ccf9e4ad4ec661b66306ab38512c
Andrew Duthie <andrew@andrewduthie.com>
2018-08-27 16:00:57 -0400

    Editor: Correct missing selector cache dependencies

--- packages/editor/src/store/selectors.js
+++ packages/editor/src/store/selectors.js
@@ -573,6 +573,8 @@ export const getBlocks = createSelector(
 	( state ) => [
 		state.editor.present.blockOrder,
 		state.editor.present.blocksByClientId,
+		state.editor.present.edits.meta,
+		state.currentPost.meta,
 	]
 );
 
@@ -1398,9 +1400,8 @@ export const getEditedPostContent = createSelector(
 		return serialize( blocks );
 	},
 	( state ) => [
+		...getBlocks.getDependants( state ),
 		state.editor.present.edits.content,
-		state.editor.present.blocksByClientId,
-		state.editor.present.blockOrder,
 	],
 );
 
